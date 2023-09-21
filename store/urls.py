92b4627e46738cebcf0b3e4ce60ab6b1ce08415b
hossein.htt <hossein.taybi@yahoo.com>
2023-09-21 16:33:00 +0330

    detail view page added with its template, and also context processor created

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ app_name = 'store'
 urlpatterns = [
     path('', StoreView.as_view(), name='store'),
     path('<slug:category_slug>/', StoreView.as_view(), name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from django.shortcuts import get_object_or_404
+from django.http import Http404
 from store.models import Product
 from category.models import Category
 
@@ -22,4 +23,33 @@ class StoreView(TemplateView):
             'count': product_count,
         }
 
+        return context
+    
+class ProductDetailView(TemplateView):
+    template_name = 'store/product_detail.html'
+
+    def get_object(self, queryset=None):
+        """
+        Retrieve the product based on category_slug and product_slug.
+        Raises Http404 if the product is not found.
+        """
+        # Get the category_slug and product_slug from URL parameters
+        category_slug = self.kwargs.get('category_slug')
+        product_slug = self.kwargs.get('product_slug')
+
+        try:
+            # Retrieve the product with the given slugs
+            product = Product.objects.get(category__slug=category_slug, slug=product_slug, is_available=True)
+        except Product.DoesNotExist:
+            # If the product does not exist, raise a 404 error
+            raise Http404("Product not found")
+
+        return product
+
+    def get_context_data(self, **kwargs):
+        # Use the retrieved product in the context
+        product = self.get_object()
+        context = {
+            'product': product,
+        }
         return context
